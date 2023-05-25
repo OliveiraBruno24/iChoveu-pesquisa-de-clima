65260aa408d5b7997ecaf516f48ade7c97fe052e
Bruno de Oliveira <brunodeoliveira2000@hotmail.com>
2023-05-25 16:29:43 -0300

    udando de maquina

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,18 @@
 // Remova os comentários a medida que for implementando as funções
+const TOKEN = import.meta.env.VITE_TOKEN;
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+const inputEl = document.querySelector('#search-input');
+export const searchCities = async (term) => {
+  term = inputEl.value;
+  try {
+    const response = await fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`);
+    const data = await response.json();
+    return data;
+  } catch {
+    if (data.length === 0) {
+      return error.message; // como eu sei oq é esse erro? sempre vejo usarem assim mas n sei de onde busca essa msg
+    }
+  }
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
