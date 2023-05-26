7b9833d67a708a97e9af60f390bdbf181f3ed63b
Bruno de Oliveira <brunodeoliveira2000@hotmail.com>
2023-05-25 23:05:07 -0300

    essa eu pensei que não ia dar... mas deu, graças a Deus 🙏

--- src/helpers/pageFunctions.js
+++ src/helpers/pageFunctions.js
@@ -1,5 +1,6 @@
 import { searchCities, getWeatherByCity } from './weatherAPI';
 
+// const TOKEN = import.meta.env.VITE_TOKEN;
 /**
  * Cria um elemento HTML com as informações passadas
  */
@@ -77,7 +78,7 @@ export function showForecast(forecastList) {
  * Recebe um objeto com as informações de uma cidade e retorna um elemento HTML
  */
 export function createCityElement(cityInfo) {
-  const { name, country, temp, condition, icon /* , url */ } = cityInfo;
+  const { name, country, temp, condition, icon } = cityInfo;
 
   const cityElement = createElement('li', 'city');
 
@@ -110,17 +111,41 @@ export function createCityElement(cityInfo) {
 /**
  * Lida com o evento de submit do formulário de busca
  */
+
 export async function handleSearch(event) {
   event.preventDefault();
   clearChildrenById('cities');
-
-  const searchInput = document.getElementById('search-input');
-  const searchValue = searchInput.value;
-  searchCities(searchValue);
-
-  const infos = await searchCities(searchValue);
-
-  infos.forEach((Objects) => {
-    getWeatherByCity(Objects.url);
-  });
+  try {
+    const searchInput = document.getElementById('search-input');
+    const searchValue = searchInput.value;
+    // busca dados da cidade na API
+    const cityData = await searchCities(searchValue);
+    // mapeia os dados das cidades que peguei em cityData e chama a função pra pegar os dados do clima da cidade.
+    const weatherPromises = cityData.map((city) => getWeatherByCity(city.url));
+    // espera todas as promessas anteriores.
+    const weatherData = await Promise.all(weatherPromises);
+    // aqui eu quero 'casar' as informações da cidade com as do clima e extrair as q me interessam.
+    const citiesInfo = cityData.map((city, index) => {
+      return {
+        country: city.country,
+        name: city.name,
+        temp: weatherData[index].temp,
+        condition: weatherData[index].condition,
+        icon: weatherData[index].icon,
+      };
+    });
+    // aqui eu pego o elemento que tem o id cities, que é a lista de cidades da página.
+    const citiesList = document.getElementById('cities');
+    // itero em cada objeto que 'casei' em citiesInfo e coloco como filho do
+    citiesInfo.forEach((city) => {
+      // createCity... Recebe um objeto com as informações de uma cidade e retorna um elemento HTML.
+      const cityElement = createCityElement(city);
+      // então aqui, citiesLint é meu ul e está recebendo como 'filho' o objeto de cidades. Que 'casei' anteriormente
+      citiesList.appendChild(cityElement);
+    });
+  } catch (error) {
+    window.alert(error.message);
+  }
 }
+
+// essa fritou os miolos kkkk tive q deixar as anotações pra n me perder
